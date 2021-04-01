f61e4672eeaa37909eed9c41a60aaf3d67f9bd32
SaiVirinchi <52707426+SaiVirinchi@users.noreply.github.com>
2021-04-01 21:06:21 +0530

    Create PINN_Diffusion.py

---
+++ PINN_Diffusion.py
@@ -0,0 +1,317 @@
+!pip install pyDOE
+
+%tensorflow_version 1.x
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+from scipy.interpolate import griddata
+from pyDOE import lhs
+from mpl_toolkits.mplot3d import Axes3D
+import time
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+np.random.seed(1234)
+tf.set_random_seed(1234)
+
+
+class PhysicsInformedNN:
+    # Initialize the class
+    def __init__(self, x0, u0, tb, X_f, layers, lb, ub):
+        
+        X0 = np.concatenate((x0, 0*x0), 1) # (x0, 0)
+        X_lb = np.concatenate((0*tb + lb[0], tb), 1) # (lb[0], tb)
+        X_ub = np.concatenate((0*tb + ub[0], tb), 1) # (ub[0], tb)
+        
+        self.lb = lb
+        self.ub = ub
+               
+        self.x0 = X0[:,0:1]
+        self.t0 = X0[:,1:2]
+
+        self.x_lb = X_lb[:,0:1]
+        self.t_lb = X_lb[:,1:2]
+
+        self.x_ub = X_ub[:,0:1]
+        self.t_ub = X_ub[:,1:2]
+        
+        self.x_f = X_f[:,0:1]
+        self.t_f = X_f[:,1:2]
+        
+        self.u0 = u0
+        
+        # Initialize NNs
+        self.layers = layers
+        self.weights, self.biases = self.initialize_NN(layers)
+        
+        # tf Placeholders        
+        self.x0_tf = tf.placeholder(tf.float32, shape=[None, self.x0.shape[1]])
+        self.t0_tf = tf.placeholder(tf.float32, shape=[None, self.t0.shape[1]])
+        
+        self.u0_tf = tf.placeholder(tf.float32, shape=[None, self.u0.shape[1]])
+        
+        self.x_lb_tf = tf.placeholder(tf.float32, shape=[None, self.x_lb.shape[1]])
+        self.t_lb_tf = tf.placeholder(tf.float32, shape=[None, self.t_lb.shape[1]])
+        
+        self.x_ub_tf = tf.placeholder(tf.float32, shape=[None, self.x_ub.shape[1]])
+        self.t_ub_tf = tf.placeholder(tf.float32, shape=[None, self.t_ub.shape[1]])
+        
+        self.x_f_tf = tf.placeholder(tf.float32, shape=[None, self.x_f.shape[1]])
+        self.t_f_tf = tf.placeholder(tf.float32, shape=[None, self.t_f.shape[1]])
+
+        # tf Graphs
+        self.u0_pred, _ = self.net_uv(self.x0_tf, self.t0_tf)
+        self.u_lb_pred, self.u_x_lb_pred = self.net_uv(self.x_lb_tf, self.t_lb_tf)
+        self.u_ub_pred, self.u_x_ub_pred = self.net_uv(self.x_ub_tf, self.t_ub_tf)
+        self.f_u_pred = self.net_f_uv(self.x_f_tf, self.t_f_tf)
+        
+        # Loss
+        self.loss = tf.reduce_mean(tf.square(self.u0_tf - self.u0_pred)) + \
+                    tf.reduce_mean(tf.square(self.u_lb_pred)) + \
+                    tf.reduce_mean(tf.square(self.u_ub_pred-100)) + \
+                    tf.reduce_mean(tf.square(self.f_u_pred))
+        
+        # Optimizers
+        self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.loss, 
+                                                                method = 'L-BFGS-B', 
+                                                                options = {'maxiter': 50000,
+                                                                           'maxfun': 50000,
+                                                                           'maxcor': 50,
+                                                                           'maxls': 50,
+                                                                           'ftol' :1e-2})# 1.0* np.finfo(float).eps})
+    
+        self.optimizer_Adam = tf.train.AdamOptimizer()
+        self.train_op_Adam = self.optimizer_Adam.minimize(self.loss)
+                
+        # tf session
+        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                                     log_device_placement=True))
+        
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+              
+    def initialize_NN(self, layers):        
+        weights = []
+        biases = []
+        num_layers = len(layers) 
+        for l in range(0,num_layers-1):
+            W = self.xavier_init(size=[layers[l], layers[l+1]])
+            b = tf.Variable(tf.zeros([1,layers[l+1]], dtype=tf.float32), dtype=tf.float32)
+            weights.append(W)
+            biases.append(b)        
+        return weights, biases
+        
+    def xavier_init(self, size):
+        in_dim = size[0]
+        out_dim = size[1]        
+        xavier_stddev = np.sqrt(2/(in_dim + out_dim))
+        return tf.Variable(tf.truncated_normal([in_dim, out_dim], stddev=xavier_stddev), dtype=tf.float32)
+    
+    def neural_net(self, X, weights, biases):
+        num_layers = len(weights) + 1
+        
+        H = 2.0*(X - self.lb)/(self.ub - self.lb) - 1.0
+        for l in range(0,num_layers-2):
+            W = weights[l]
+            b = biases[l]
+            H = tf.tanh(tf.add(tf.matmul(H, W), b))
+        W = weights[-1]
+        b = biases[-1]
+        Y = tf.add(tf.matmul(H, W), b)
+        return Y
+    
+    def net_uv(self, x, t):
+        X = tf.concat([x,t],1)
+        
+        uv = self.neural_net(X, self.weights, self.biases)
+        u = uv[:,0:1]
+        
+        u_x = tf.gradients(u, x)[0]
+
+        return u, u_x
+
+    def net_f_uv(self, x, t):
+        u, u_x = self.net_uv(x,t)
+        global nu 
+        u_t = tf.gradients(u, t)[0]
+        u_xx = tf.gradients(u_x, x)[0]
+        
+        f_u = u_t - nu*u_xx  
+        
+        return f_u
+    
+    def callback(self, loss):
+        print('Loss:', loss)
+        
+    def train(self, nIter):
+        
+        tf_dict = {self.x0_tf: self.x0, self.t0_tf: self.t0,
+                   self.u0_tf: self.u0,
+                   self.x_lb_tf: self.x_lb, self.t_lb_tf: self.t_lb,
+                   self.x_ub_tf: self.x_ub, self.t_ub_tf: self.t_ub,
+                   self.x_f_tf: self.x_f, self.t_f_tf: self.t_f}
+        
+        start_time = time.time()
+        for it in range(nIter):
+            self.sess.run(self.train_op_Adam, tf_dict)
+            
+            # Print
+            if it % 10 == 0:
+                elapsed = time.time() - start_time
+                loss_value = self.sess.run(self.loss, tf_dict)
+                print('It: %d, Loss: %.3e, Time: %.2f' % 
+                      (it, loss_value, elapsed))
+                start_time = time.time()
+                                                                                                                          
+        self.optimizer.minimize(self.sess, 
+                                feed_dict = tf_dict,         
+                                fetches = [self.loss], 
+                                loss_callback = self.callback)        
+                                    
+    
+    def predict(self, X_star):
+        
+        tf_dict = {self.x0_tf: X_star[:,0:1], self.t0_tf: X_star[:,1:2]}
+        
+        u_star = self.sess.run(self.u0_pred, tf_dict)   
+        
+        
+        tf_dict = {self.x_f_tf: X_star[:,0:1], self.t_f_tf: X_star[:,1:2]}
+        
+        f_u_star = self.sess.run(self.f_u_pred, tf_dict)
+               
+        return u_star,f_u_star
+            
+if __name__ == "__main__": 
+     
+    noise = 0.0        
+    
+    # Doman bounds
+    # Doman bounds
+    lb = np.array([0, 0])
+    ub = np.array([10, 20])
+
+    N0 = 10
+    N_b = 10
+    N_f = 2000
+    layers = [2, 100, 100, 100, 100, 2]
+    nu = 2
+        
+    from scipy import special
+    
+    
+    def diffusion(nt, nx, tmax, xmax, nu):
+      """
+      Returns the velocity field and distance for 1D linear convection
+      """
+      # Increments
+      dt = tmax/(nt-1)
+      dx = xmax/(nx-1)
+      plate_length = xmax
+      max_iter_time = tmax
+
+      alpha = nu
+      delta_x = dx
+      delta_t = (delta_x ** 2)/(4 * alpha)
+      
+      x = np.zeros(nx)
+      t = np.zeros(nt)
+
+      #delta_t = (delta_x ** 2)/(4 * alpha)
+      gamma = (alpha * delta_t) / (delta_x ** 2)
+
+      # Initialize solution: the grid of u(k, i)
+      u = np.empty((nx, nt))
+
+      # Initial condition everywhere inside the grid
+      u_initial = np.random.uniform(low=28.5, high=55.5, size=(nx))
+
+      # Boundary conditions
+      u_top = 100
+      u_bottom = 0.0
+
+      # Set the initial condition
+      u[:,0] = u_initial
+
+      # Set the boundary conditions
+      u[(nx-1):,:] = u_top
+      u[:1,:] = u_bottom
+
+      if dt <= (dx**2)/(2*alpha):
+        print("you are lucky")
+      else: 
+        print("hmmm",dt,(dx**2)/(4*alpha))
+      for k in range(0, nt-1):
+        for i in range(1, nx-1):
+          u[i,k + 1] = gamma * (u[i+1][k] + u[i-1][k] - 2*u[i][k]) + u[i][k]
+
+
+      # X Loop
+      for i in range(0,nx):
+          x[i] = i*dx
+      # T Loop
+      for i in range(0,nt):
+          t[i] = i*dt
+      return u, x, t
+  
+    Exact, xa, ta = diffusion(50, 10, 49, 9, nu)
+    x = xa[:,None]
+    t = ta[:,None]
+    Exact_u = Exact
+    print(np.shape(Exact),np.shape(x),np.shape(t))
+    X, T = np.meshgrid(x,t)
+    
+    X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None]))
+    u_star = Exact_u.T.flatten()[:,None]
+    h_star = Exact_h.T.flatten()[:,None]
+    
+    ###########################
+    
+    idx_x = np.random.choice(x.shape[0], N0, replace=False)
+    x0 = x[idx_x,:]
+    u0 = Exact_u[idx_x,0:1]
+    
+
+    idx_t = np.random.choice(t.shape[0], N_b, replace=False)
+    tb = t[idx_t,:]
+    
+    X_f = lb + (ub-lb)*lhs(2, N_f)
+
+    model = PhysicsInformedNN(x0, u0, tb, X_f, layers, lb, ub)
+
+             
+    start_time = time.time()                
+    model.train(500)
+    elapsed = time.time() - start_time                
+    print('Training time: %.4f' % (elapsed))
+    
+        
+    u_pred, f_u_pred = model.predict(X_star)
+  
+            
+    error_u = np.linalg.norm(u_star-u_pred,2)/np.linalg.norm(u_star,2)
+    print('Error u: %e' % (error_u))
+
+    
+    U_pred = griddata(X_star, u_pred.flatten(), (X, T), method='cubic')
+
+    FU_pred = griddata(X_star, f_u_pred.flatten(), (X, T), method='cubic')   
+    
+    
+    ######################################################################
+    ############################# Plotting ###############################
+    ######################################################################    
+    
+      
+    i = 2 # value of t
+
+    plt.plot(x,Exact_u[:,i], linewidth = 2,label = 'Exact')       
+    plt.plot(x,U_pred[i,:], 'r', linewidth = 2, label = 'Prediction')
+    plt.xlabel('x')
+    plt.ylabel('h(t,x)')
+    plt.legend(loc='upper center')
+    plt.show()      
+     
